c5276201f9206cae46152333a2fa544adbe46f18
PanduSama <pandurosama@gmail.com>
2025-07-28 15:46:44 -0300

    Añadir modelo Post y migraciones iniciales

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     contenido = models.TextField()
-    fecha_publicaciones = models.DateTimeField(auto_now_add=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
