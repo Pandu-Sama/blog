24f7206919af1281828507b5ee524f1daafacb16
PanduSama <pandurosama@gmail.com>
2025-07-28 20:09:03 -0300

    Añadir modelo author y relaciòn ForeignKey en Post

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     contenido = models.TextField()
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
