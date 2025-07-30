bb00b14bf5cb02a703e3760e40ced54c78053855
PanduSama <pandurosama@gmail.com>
2025-07-30 17:06:27 -0300

    Añadir vista y plantillas para detalles de post con URLs dinàmicas

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import HttpResponse
 
@@ -6,4 +6,7 @@ def index(request):
     posts = Post.objects.all().order_by('-fecha_publicacion')
     return render(request, 'blog/index.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
