df598ef2feb28b83546c4523409a3744df9f1c98
PanduSama <pandurosama@gmail.com>
2025-07-31 21:46:54 -0300

    Mini-blog funcional: modelos, vistas, plantillas y admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Author, Post
 
-admin.site.register(Author)
-admin.site.register(Post)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'author', 'fecha_publicacion')
+    list_filter = ('titulo', 'fecha_publicacion')
+    search_fields = ('titulo', 'contenido')
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
 ]
